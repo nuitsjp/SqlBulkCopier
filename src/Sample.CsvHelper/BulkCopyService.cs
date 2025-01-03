a87cd0feb575079e5d116d10180ce8092c3bbca0
Atsushi Nakamura <16175660+nuitsjp@users.noreply.github.com>
2025-01-04 06:32:27 +0900

    wip

--- src/Sample.CsvHelper/BulkCopyService.cs
+++ src/Sample.CsvHelper/BulkCopyService.cs
@@ -5,6 +5,9 @@ using SqlBulkCopier.Hosting;
 
 namespace Sample.CsvHelper;
 
+/// <summary>
+/// A service that uses the bulk copier to write data to the database.
+/// </summary>
 public class BulkCopyService(
     IBulkCopier bulkCopier, 
     SqlConnectionProvider sqlConnectionProvider,
@@ -12,8 +15,13 @@ public class BulkCopyService(
 {
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
+        // Open a connection to the database
         await using var connection = await sqlConnectionProvider.OpenAsync();
+
+        // Write data to the database using the bulk copier
         await using Stream stream = File.OpenRead(@"Assets\Customer.csv");
+
+        // Bulk copy to the database
         await bulkCopier.WriteToServerAsync(connection, stream, Encoding.UTF8);
 
         // Stop the application when the task is completed
