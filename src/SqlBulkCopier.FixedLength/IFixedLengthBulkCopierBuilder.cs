41e44a5ac7d1dc5f6fd781be358ddff48299dd11
Atsushi Nakamura <16175660+nuitsjp@users.noreply.github.com>
2025-01-20 14:16:38 +0900

    wip

--- src/SqlBulkCopier.FixedLength/IFixedLengthBulkCopierBuilder.cs
+++ src/SqlBulkCopier.FixedLength/IFixedLengthBulkCopierBuilder.cs
@@ -2,50 +2,8 @@ using FixedLengthHelper;
 
 namespace SqlBulkCopier.FixedLength;
 
-public interface IFixedLengthBulkCopierBuilder : IBulkCopierBuilder
+public interface IFixedLengthBulkCopierBuilder : IBulkCopierBuilder<IFixedLengthBulkCopierBuilder>
 {
-    /// <summary>
-    /// Set max retry count
-    /// </summary>
-    /// <param name="value"></param>
-    /// <returns></returns>
-    IFixedLengthBulkCopierBuilder SetMaxRetryCount(int value);
-
-    /// <summary>
-    /// Set retry delay
-    /// </summary>
-    /// <param name="value"></param>
-    /// <returns></returns>
-    IFixedLengthBulkCopierBuilder SetInitialDelay(TimeSpan value);
-
-    /// <summary>
-    /// Set truncate before bulk insert
-    /// </summary>
-    /// <param name="value"></param>
-    /// <returns></returns>
-    IFixedLengthBulkCopierBuilder SetTruncateBeforeBulkInsert(bool value);
-
-    /// <summary>
-    /// Set use exponential backoff
-    /// </summary>
-    /// <param name="value"></param>
-    /// <returns></returns>
-    IFixedLengthBulkCopierBuilder SetUseExponentialBackoff(bool value);
-
-    /// <summary>
-    /// Set batch size
-    /// </summary>
-    /// <param name="value"></param>
-    /// <returns></returns>
-    IFixedLengthBulkCopierBuilder SetBatchSize(int value);
-
-    /// <summary>
-    /// Set notify after
-    /// </summary>
-    /// <param name="value"></param>
-    /// <returns></returns>
-    IFixedLengthBulkCopierBuilder SetNotifyAfter(int value);
-    IFixedLengthBulkCopierBuilder SetDefaultColumnContext(Action<IColumnContext> c);
     IFixedLengthBulkCopierBuilder SetRowFilter(Predicate<IFixedLengthReader> rowFilter);
     IFixedLengthBulkCopierBuilder AddColumnMapping(string dbColumnName, int offsetBytes, int lengthBytes);
     IFixedLengthBulkCopierBuilder AddColumnMapping(string dbColumnName, int offsetBytes, int lengthBytes, Action<IColumnContext> c);
