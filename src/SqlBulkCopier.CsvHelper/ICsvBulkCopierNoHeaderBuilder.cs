5d10a507f030301db7a9caacea80e51f42fdcc27
Atsushi Nakamura <16175660+nuitsjp@users.noreply.github.com>
2025-01-20 13:39:06 +0900

    wip

--- src/SqlBulkCopier.CsvHelper/ICsvBulkCopierNoHeaderBuilder.cs
+++ src/SqlBulkCopier.CsvHelper/ICsvBulkCopierNoHeaderBuilder.cs
@@ -2,56 +2,8 @@
 
 namespace SqlBulkCopier.CsvHelper;
 
-public interface ICsvBulkCopierNoHeaderBuilder : IBulkCopierBuilder
+public interface ICsvBulkCopierNoHeaderBuilder : IBulkCopierBuilder<ICsvBulkCopierNoHeaderBuilder>
 {
-    /// <summary>
-    /// Set max retry count
-    /// </summary>
-    /// <param name="value"></param>
-    /// <returns></returns>
-    ICsvBulkCopierNoHeaderBuilder SetMaxRetryCount(int value);
-
-    /// <summary>
-    /// Set retry delay
-    /// </summary>
-    /// <param name="value"></param>
-    /// <returns></returns>
-    ICsvBulkCopierNoHeaderBuilder SetInitialDelay(TimeSpan value);
-
-    /// <summary>
-    /// Set truncate before bulk insert
-    /// </summary>
-    /// <param name="value"></param>
-    /// <returns></returns>
-    ICsvBulkCopierNoHeaderBuilder SetTruncateBeforeBulkInsert(bool value);
-
-    /// <summary>
-    /// Set use exponential backoff
-    /// </summary>
-    /// <param name="value"></param>
-    /// <returns></returns>
-    ICsvBulkCopierNoHeaderBuilder SetUseExponentialBackoff(bool value);
-
-    /// <summary>
-    /// Set batch size
-    /// </summary>
-    /// <param name="value"></param>
-    /// <returns></returns>
-    ICsvBulkCopierNoHeaderBuilder SetBatchSize(int value);
-
-    /// <summary>
-    /// Set notify after
-    /// </summary>
-    /// <param name="value"></param>
-    /// <returns></returns>
-    ICsvBulkCopierNoHeaderBuilder SetNotifyAfter(int value);
-
-    /// <summary>
-    /// Setup default column context
-    /// </summary>
-    /// <param name="c"></param>
-    /// <returns></returns>
-    ICsvBulkCopierNoHeaderBuilder SetDefaultColumnContext(Action<IColumnContext> c);
     /// <summary>
     /// Set row filter
     /// </summary>
--- src/SqlBulkCopier/IBulkCopierBuilder.cs
+++ src/SqlBulkCopier/IBulkCopierBuilder.cs
@@ -9,3 +9,56 @@ public interface IBulkCopierBuilder
     IBulkCopier Build(string connectionString, SqlBulkCopyOptions copyOptions);
     IBulkCopier Build(SqlConnection connection, SqlBulkCopyOptions copyOptions, SqlTransaction externalTransaction);
 }
+
+public interface IBulkCopierBuilder<out TBuilder> : IBulkCopierBuilder
+    where TBuilder : IBulkCopierBuilder<TBuilder>
+{
+    /// <summary>
+    /// Set max retry count
+    /// </summary>
+    /// <param name="value"></param>
+    /// <returns></returns>
+    TBuilder SetMaxRetryCount(int value);
+
+    /// <summary>
+    /// Set retry delay
+    /// </summary>
+    /// <param name="value"></param>
+    /// <returns></returns>
+    TBuilder SetInitialDelay(TimeSpan value);
+
+    /// <summary>
+    /// Set truncate before bulk insert
+    /// </summary>
+    /// <param name="value"></param>
+    /// <returns></returns>
+    TBuilder SetTruncateBeforeBulkInsert(bool value);
+
+    /// <summary>
+    /// Set use exponential backoff
+    /// </summary>
+    /// <param name="value"></param>
+    /// <returns></returns>
+    TBuilder SetUseExponentialBackoff(bool value);
+
+    /// <summary>
+    /// Set batch size
+    /// </summary>
+    /// <param name="value"></param>
+    /// <returns></returns>
+    TBuilder SetBatchSize(int value);
+
+    /// <summary>
+    /// Set notify after
+    /// </summary>
+    /// <param name="value"></param>
+    /// <returns></returns>
+    TBuilder SetNotifyAfter(int value);
+
+    /// <summary>
+    /// Setup default column context
+    /// </summary>
+    /// <param name="c"></param>
+    /// <returns></returns>
+    TBuilder SetDefaultColumnContext(Action<IColumnContext> c);
+}
