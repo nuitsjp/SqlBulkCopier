bdb3b8289cc4c9df88fdcad39e68c5af4aafb892
Atsushi Nakamura <16175660+nuitsjp@users.noreply.github.com>
2025-01-20 13:42:03 +0900

    wip

---
+++ src/SqlBulkCopier.CsvHelper/ICsvBulkCopierBuilder.cs
@@ -0,0 +1,14 @@
+﻿using CsvHelper;
+
+namespace SqlBulkCopier.CsvHelper;
+
+public interface ICsvBulkCopierBuilder<out TBuilder> : IBulkCopierBuilder<TBuilder>
+    where TBuilder : ICsvBulkCopierBuilder<TBuilder>
+{
+    /// <summary>
+    /// Set row filter
+    /// </summary>
+    /// <param name="rowFilter"></param>
+    /// <returns></returns>
+    TBuilder SetRowFilter(Predicate<CsvReader> rowFilter);
+}
--- src/SqlBulkCopier.CsvHelper/ICsvBulkCopierNoHeaderBuilder.cs
+++ src/SqlBulkCopier.CsvHelper/ICsvBulkCopierNoHeaderBuilder.cs
@@ -1,17 +1,4 @@
-﻿using CsvHelper;
-
-namespace SqlBulkCopier.CsvHelper;
-
-public interface ICsvBulkCopierBuilder<out TBuilder> : IBulkCopierBuilder<TBuilder>
-    where TBuilder : ICsvBulkCopierBuilder<TBuilder>
-{
-    /// <summary>
-    /// Set row filter
-    /// </summary>
-    /// <param name="rowFilter"></param>
-    /// <returns></returns>
-    TBuilder SetRowFilter(Predicate<CsvReader> rowFilter);
-}
+﻿namespace SqlBulkCopier.CsvHelper;
 
 public interface ICsvBulkCopierNoHeaderBuilder : ICsvBulkCopierBuilder<ICsvBulkCopierNoHeaderBuilder>
 {
