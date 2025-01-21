7750105bd17806d9a8fa3dc8fb724ed7e1631913
Atsushi Nakamura <16175660+nuitsjp@users.noreply.github.com>
2025-01-21 20:47:27 +0900

    wip

--- src/SqlBulkCopier/BulkCopier.cs
+++ src/SqlBulkCopier/BulkCopier.cs
@@ -3,16 +3,39 @@ using Microsoft.Data.SqlClient;
 
 namespace SqlBulkCopier;
 
+/// <summary>
+/// Bulk copier.
+/// </summary>
 public class BulkCopier : IBulkCopier
 {
+    /// <summary>
+    /// Event when rows copied.
+    /// </summary>
     public event SqlRowsCopiedEventHandler? SqlRowsCopied;
 
+    /// <summary>
+    /// SQL bulk copy.
+    /// </summary>
     private readonly SqlBulkCopy _sqlBulkCopy;
 
+    /// <summary>
+    /// Connection string.
+    /// </summary>
     private readonly string? _connectionString;
+
+    /// <summary>
+    /// Connection.
+    /// </summary>
     private readonly SqlConnection? _connection;
+
+    /// <summary>
+    /// External transaction.
+    /// </summary>
     private readonly SqlTransaction? _externalTransaction;
 
+    /// <summary>
+    /// Constructor.
+    /// </summary>
     public BulkCopier(
         string destinationTableName,
         IDataReaderBuilder dataReaderBuilder,
@@ -24,6 +47,9 @@ public class BulkCopier : IBulkCopier
         _externalTransaction = null;
     }
 
+    /// <summary>
+    /// Constructor.
+    /// </summary>
     public BulkCopier(
         string destinationTableName,
         IDataReaderBuilder dataReaderBuilder,
@@ -35,6 +61,9 @@ public class BulkCopier : IBulkCopier
         _externalTransaction = null;
     }
 
+    /// <summary>
+    /// Constructor.
+    /// </summary>
     public BulkCopier(
         string destinationTableName,
         IDataReaderBuilder dataReaderBuilder, 
@@ -47,6 +76,9 @@ public class BulkCopier : IBulkCopier
         _externalTransaction = null;
     }
 
+    /// <summary>
+    /// Constructor.
+    /// </summary>
     public BulkCopier(
         string destinationTableName,
         IDataReaderBuilder dataReaderBuilder, 
@@ -60,6 +92,9 @@ public class BulkCopier : IBulkCopier
         _externalTransaction = externalTransaction;
     }
 
+    /// <summary>
+    /// Constructor.
+    /// </summary>
     private BulkCopier(
         string destinationTableName,
         SqlBulkCopy sqlBulkCopy,
@@ -73,30 +108,73 @@ public class BulkCopier : IBulkCopier
         _sqlBulkCopy.SqlRowsCopied += SqlBulkCopyOnSqlRowsCopied;
     }
 
+    /// <summary>
+    /// IDataReaderBuilder
+    /// </summary>
     public IDataReaderBuilder DataReaderBuilder { get; init; }
 
+    /// <summary>
+    /// Destination table name of bulk insert.
+    /// </summary>
     public string DestinationTableName => _sqlBulkCopy.DestinationTableName;
 
+    /// <summary>
+    /// Maximum retry count.
+    /// </summary>
     public int MaxRetryCount { get; set; }
+
+    /// <summary>
+    /// Truncate before bulk insert.
+    /// </summary>
     public bool TruncateBeforeBulkInsert { get; set; }
+
+    /// <summary>
+    /// Use exponential backoff. Retry delay time is doubled.
+    /// </summary>
     public bool UseExponentialBackoff { get; set; }
+
+    /// <summary>
+    /// Retry initial delay time.
+    /// </summary>
     public TimeSpan InitialDelay { get; set; }
 
+    /// <summary>
+    /// Number of rows in each batch. Refer to SqlBulkCopy.BatchSize.
+    /// </summary>
     public int BatchSize
     {
         get => _sqlBulkCopy.BatchSize;
         set => _sqlBulkCopy.BatchSize = value;
     }
 
+    /// <summary>
+    /// Number of rows in each batch. Refer to SqlBulkCopy.NotifyAfter.
+    /// </summary>
     public int NotifyAfter
     {
         get => _sqlBulkCopy.NotifyAfter;
         set => _sqlBulkCopy.NotifyAfter = value;
     }
 
+    /// <summary>
+    /// Number of rows copied. Refer to SqlBulkCopy.RowsCopied.
+    /// </summary>
     public int RowsCopied => _sqlBulkCopy.RowsCopied;
+
+    /// <summary>
+    /// Number of rows copied. Refer to SqlBulkCopy.RowsCopied64.
+    /// </summary>
     public long RowsCopied64 => _sqlBulkCopy.RowsCopied64;
 
+    /// <summary>
+    /// Write to server asynchronously.
+    /// </summary>
+    /// <param name="stream"></param>
+    /// <param name="encoding"></param>
+    /// <param name="timeout"></param>
+    /// <returns></returns>
+    /// <exception cref="InvalidOperationException"></exception>
+    /// <exception cref="Exception"></exception>
     public async Task WriteToServerAsync(Stream stream, Encoding encoding, TimeSpan timeout)
     {
         _sqlBulkCopy.BulkCopyTimeout = (int)timeout.TotalSeconds;
@@ -142,7 +220,7 @@ public class BulkCopier : IBulkCopier
                 if (currentRetryCount > MaxRetryCount)
                 {
                     // When the retry count exceeds the maximum, throw an exception
-                    throw new Exception($"BulkCopier failed after {currentRetryCount - 1} retries.", ex);
+                    throw new InvalidOperationException($"BulkCopier failed after {currentRetryCount - 1} retries.", ex);
                 }
 
                 // When use exponential backoff, double the delay time
